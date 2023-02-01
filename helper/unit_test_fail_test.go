cc5700110a1a12064972df8219e490c7e84d2126
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-02-01 14:04:39 +0700

    uni test fail

---
+++ helper/unit_test_fail_test.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFail(t *testing.T) {
+	result := HelloWorld("Salah")
+
+	if result != "Hello Fajar" {
+		t.Fail()
+	}
+	fmt.Println("Test Fail Done")
+}
+
+func TestFailNow(t *testing.T) {
+	result := HelloWorld("Fajar")
+	if result != "Hello  Fajar" {
+		t.FailNow()
+	}
+	fmt.Println("Test Fail Now")
+}
