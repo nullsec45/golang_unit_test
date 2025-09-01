c904acc85d07a3c0a75c5146515027c40c712fa4
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-01 13:08:11 +0700

    feat : golang benchmark unit test.

--- helper/hello-world_test.go
+++ helper/hello-world_test.go
@@ -2,6 +2,62 @@ package helper
 
 import "testing"
 
+
+func HelloWorld(name string) string {
+	return "Hello " + name
+}
+
+func BenchmarkTable(b *testing.B){
+	benchmarks := []struct {
+		name string
+		request string
+	}{
+		{
+			name: "Rama",
+			request: "Rama",
+		},
+		{	
+			name: "Fajar",
+			request: "Fajar",
+		},
+	}
+
+	for _, benchmark := range benchmarks{
+		b.Run(benchmark.name, func(b *testing.B){
+			for i := 0; i < b.N; i++{
+				HelloWorld(benchmark.request)
+			}
+		})
+	}
+}
+
+func BenchMarkSub(b *testing.B){
+	b.Run("Rama", func(b *testing.B){
+		for i := 0; i < b.N; i++{
+			HelloWorld("Rama")
+		}
+	})
+	b.Run("Fajar", func(b *testing.B){
+		for i := 0; i < b.N; i++{
+			HelloWorld("Fajar")
+		}
+	}) 
+}
+
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Rama")
+	}
+}
+
+func BenchmarkHelloWorldFajar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Fajar")
+	}
+}
+
+
+
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Fajar")
 
